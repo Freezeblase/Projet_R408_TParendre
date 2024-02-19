e96eb1e5b4c18dd0f0d453aa1a84e8ff511bfbc1
XionX111 <159144336+XionX111@users.noreply.github.com>
2024-02-19 17:19:57 +0100

    Update script.js
    j'ai rajouté la partie javascript effectué dans mon coins dans le fichier js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-/**/
+    function openOverlay(title, imageUrl, description) {
+        document.getElementById('overlay-text').innerHTML = `<strong>${title}</strong><br>${description}`;
+        document.getElementById('overlay-image').src = imageUrl;
+        document.querySelector('.overlay').style.display = 'flex';
+    }
+    
+    function closeOverlay() {
+        document.querySelector('.overlay').style.display = 'none';
+    }
